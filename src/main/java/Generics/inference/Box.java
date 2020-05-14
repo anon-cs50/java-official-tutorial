d833fc766d91743c1424673fbc81eb483fa1681c
anon-cs50 <60882116+anon-cs50@users.noreply.github.com>
2020-05-14 18:41:16 +0530

    Add Generics type inference

---
+++ src/main/java/Generics/inference/Box.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Generics.inference;
+
+/**
+ * Generic version of the Box class.
+ *
+ * @param <T> the type of value being boxed
+ */
+public class Box<T> {
+
+    private T t;
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+}
