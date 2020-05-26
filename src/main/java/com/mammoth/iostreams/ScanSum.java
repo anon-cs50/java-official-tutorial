5410b1b24f190d60e248725bd6128d1c959b8933
anon-cs50 <60882116+anon-cs50@users.noreply.github.com>
2020-05-26 20:18:28 +0530

    Update ScanSum

--- src/main/java/com/mammoth/iostreams/ScanSum.java
+++ src/main/java/com/mammoth/iostreams/ScanSum.java
@@ -1,10 +1,36 @@
 package com.mammoth.iostreams;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ScanSum {
 
     private static final String PATH = "/Users/mammoth/Developer/java/javatutorial/src/main/java/com/mammoth/iostreams/";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner s = null;
+        double sum = 0;
 
+        try {
+            s = new Scanner(new BufferedReader(new FileReader(PATH + "usnumbers.txt")));
+            s.useLocale(Locale.US);
+
+            while (s.hasNext()) {
+                if (s.hasNextDouble()) {
+                    sum += s.nextDouble();
+                } else {
+                    s.next();
+                }
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+
+        System.out.println(sum);
     }
 }
